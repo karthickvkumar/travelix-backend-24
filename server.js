5503fda6cb150d036e7f76ab6e3ddd689300ab0b
kvelraj_concord <kvelraj@concord.net>
2023-04-09 11:51:47 +0530

    upload

---
+++ server.js
@@ -0,0 +1,160 @@
+var express = require("express");
+var cors = require("cors");
+var mysql = require("mysql");
+
+var app = express();
+
+var http = require("http").createServer(app);
+
+app.use(express.json({limit : "50mb"}));
+app.use(cors({
+  credentials: true,
+  origin: "*"
+}));
+
+var connection  = mysql.createConnection({
+  host: "db4free.net",
+  database: "travelix",
+  user: "vcentry",
+  password: "test@123",
+  port: 3306
+})
+
+// Local MySQL
+// user: "root",
+// password: "Test@123",
+
+connection.connect((error) => {
+  if(error){
+    throw error;
+  }
+
+  console.log("MySQL workbench connected");
+});
+
+
+var port = process.env.PORT || 4000;
+http.listen(port, () => {
+  console.log("Node js server is running on port 4000");
+}) 
+
+// http://localhost:4000/create/destination
+app.post("/create/destination", (request, response) => {
+  var resposneData = request.body;
+
+  var sqlQuery = `INSERT INTO destinations (destinationName, destinationLocation, destinationImage, destinationCount) VALUES ('${resposneData.destinationName}', '${resposneData.destinationLocation}', '${resposneData.destinationImage}', ${resposneData.destinationCount})`;
+
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send("Destination had been created");
+    }
+  })
+});
+
+// http://localhost:4000/list/destinations
+app.get("/list/destinations", (request, response) => {
+
+  var destinationName = request.query.destinationName;
+  var destinationLocation = request.query.destinationLocation;
+
+  if(!destinationName || destinationName == "" ){
+    var sqlQuery = `SELECT * FROM destinations;`;
+  }
+  else{
+    var sqlQuery = `SELECT * FROM destinations WHERE destinationName='${destinationName}' OR destinationLocation='${destinationLocation}'`;
+  }
+
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send(result);
+    }
+  })
+
+})
+
+// http://localhost:4000/delete/destionation/2
+app.delete("/delete/destionation/:id", (request, response) => {
+  var id = request.params.id;
+  var sqlQuery = `DELETE FROM destinations WHERE id=${id}`;
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send("Destination had been deleted");
+    }
+  })
+})
+
+// http://localhost:4000/create/hotel
+app.post("/create/hotel", (request, response) => {
+  var resposneData = request.body;
+
+  var sqlQuery = `INSERT INTO hotels (hotelName, hotelImage, hotelLocation, hotelPrice) VALUES ('${resposneData.hotelName}', '${resposneData.hotelImage}', '${resposneData.hotelLocation}', ${resposneData.hotelPrice})`;
+
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send("Hotel had been created");
+    }
+  })
+});
+
+// http://localhost:4000/list/hotels
+app.get("/list/hotels", (request, response) => {
+
+  var hotelLocation = request.query.hotelLocation;
+
+  if(hotelLocation && hotelLocation == ""){
+    var sqlQuery = `SELECT * FROM hotels;`;
+  }
+  else{
+    var sqlQuery = `SELECT * FROM hotels WHERE hotelLocation='${hotelLocation}'`;
+  }
+
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send(result);
+    }
+  })
+
+})
+
+// http://localhost:4000/delete/hotel/2
+app.delete("/delete/hotel/:id", (request, response) => {
+  var id = request.params.id;
+  var sqlQuery = `DELETE FROM hotels WHERE id=${id}`;
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send("Hotel had been deleted");
+    }
+  })
+})
+
+/*
+CREATE DATABASE travelix;
+=======================================
+USE travelix;
+
+CREATE TABLE destinations (destinationName varchar(255), destinationLocation varchar(255), destinationImage longtext, destinationCount int, id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));
+
+============================================================
+USE travelix;
+
+CREATE TABLE hotels (hotelName varchar(255), hotelImage longtext, hotelLocation varchar(255), hotelPrice int, id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));
+
+*/
